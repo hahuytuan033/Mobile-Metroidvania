efce8aa3f7050486f6e3871dfb588c6836e610bc
Huy Tuấn <163986199+hahuytuan033@users.noreply.github.com>
2025-02-26 16:03:01 +0700

    player jump and flip

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -1,12 +1,24 @@
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator anim;
+
+    [Header("Player Properties")]
     [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+
+    [Header("Collision Properties")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask groundLayer;
+    private bool isGrounded;
 
     private float xInput;
+
+    private bool facingRight = true;
+    private int facingDir = 1;
 
     private void Awake()
     {
@@ -17,15 +29,37 @@ public class PlayerManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        xInput = Input.GetAxis("Horizontal");
-        PlayerAnimation();
+        PlayerCollision();
+        PlayerInput();
         PlayerMovement();
+        Flip();
+        PlayerAnimation();
+    }
+
+    private void PlayerInput()
+    {
+        xInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.K) && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+
+    private void PlayerCollision()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundLayer);
     }
 
     private void PlayerMovement()
@@ -35,6 +69,23 @@ public class PlayerManager : MonoBehaviour
 
     private void PlayerAnimation()
     {
-        anim.SetFloat("xVeclocity", rb.linearVelocity.x);
+        anim.SetFloat("xVelocity", rb.linearVelocity.x);
+        anim.SetFloat("yVelocity", rb.linearVelocity.y);
+        anim.SetBool("isGrounded", isGrounded);
+    }
+
+    private void Flip()
+    {
+        if(xInput< 0 && facingRight || xInput > 0 && !facingRight)
+        {
+            facingDir = facingDir * -1;
+            transform.Rotate(0.0f, 180.0f, 0.0f);
+            facingRight = !facingRight;
+        }
+    }
+
+    private void OrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundCheckDistance));        
     }
 }
